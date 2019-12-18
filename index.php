e0cdd3cd70d783504431a04d4dfa43b13955341a
anthony39100 <anthonybreviglieri.ab@gmail.com>
2019-12-18 17:50:03 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require 'vendor/autoload.php';
+//rooting
+$page="home";
+if(isset($_GET['p'])){
+    $page=$_GET['p'];
+}
+//rendu du template
+$loader=new \Twig\Loader\FilesystemLoader(__DIR__.'/template');
+$twig=new\Twig\Environment($loader,[
+    'cache'=>false,
+]);
+echo $twig->render('index.html', ['name' => 'Fabien']);
+?>
